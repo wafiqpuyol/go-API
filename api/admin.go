0b1d1b1784f1aab8d9fde5b56cad44261717b9c5
wafiq <wafiqgithub775@gmail.com>
2024-12-17 06:08:40 +0600

    init commit

---
+++ api/admin.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/fulltimegodev/hotel-reservation/types"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AdminAuth(c *fiber.Ctx) error {
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return ErrUnAuthorized()
+	}
+	if !user.IsAdmin {
+		return ErrUnAuthorized()
+	}
+	return c.Next()
+}
